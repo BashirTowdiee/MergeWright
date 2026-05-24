4c3c3e9ed7db329a5d8fe0c337d51608ae094a50
BASH <39274523+BashirTowdiee@users.noreply.github.com>
2026-05-24 23:30:45 +1000

    Export app event primitives

---
+++ src/application/events/index.ts
@@ -0,0 +1,3 @@
+export type { AppEvent, AppEventBase, AppEventType, CommandFinishedEvent, CommandStartedEvent, PhaseFinishedEvent, PhaseOutputEvent, PhaseStartedEvent } from "./app-event.js";
+export type { AppEventBus, AppEventSubscriber, AppEventSubscription } from "./app-event-bus.js";
+export { InMemoryAppEventBus } from "./app-event-bus.js";
