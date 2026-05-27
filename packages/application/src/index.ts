78d97a4eccd5d0e1e9f00be1c6fef3bedb9ff143
BASH <39274523+BashirTowdiee@users.noreply.github.com>
2026-05-27 13:42:44 +1000

    Fix application boundary export collisions

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -1,22 +1,58 @@
-export * from "../../../src/application/commands/app-command.js";
-export * from "../../../src/application/commands/app-command-error.js";
-export * from "../../../src/application/commands/app-command-result.js";
-export * from "../../../src/application/commands/command-description.js";
-export * from "../../../src/application/commands/command-risk.js";
-export * from "../../../src/application/commands/command-source.js";
-export * from "../../../src/application/commands/confirmation.js";
-export * from "../../../src/application/commands/default-app-command-service.js";
-export * from "../../../src/application/commands/evented-app-command-service.js";
+export {
+  APP_COMMAND_TYPES,
+  type AddTaskCommentCommand,
+  type AppCommand,
+  type AppCommandType,
+  type ApproveStageCommand,
+  type ContinueRunCommand,
+  type ExecuteBuilderCommand,
+  type MarkTaskReviewedCommand,
+  type ReassessStagePlanCommand,
+  type RetryPhaseCommand,
+  type SelectTaskCommand,
+  type StartRunCommand,
+  type UpdateCoordinationNoteCommand
+} from "../../../src/application/commands/app-command.js";
+export { APP_COMMAND_ERROR_CODES, type AppCommandError, type AppCommandErrorCode } from "../../../src/application/commands/app-command-error.js";
+export {
+  isAppCommandFailure,
+  isAppCommandSuccess,
+  type AppCommandFailureResult,
+  type AppCommandResult,
+  type AppCommandSuccessResult
+} from "../../../src/application/commands/app-command-result.js";
+export { describeCommand, type CommandDescription } from "../../../src/application/commands/command-description.js";
+export { COMMAND_RISKS, requiresConfirmationForRisk, type CommandRisk } from "../../../src/application/commands/command-risk.js";
+export { type CommandActor, type CommandMetadata, type CommandSource } from "../../../src/application/commands/command-source.js";
+export { getCommandConfirmationState, type CommandConfirmationState } from "../../../src/application/commands/confirmation.js";
+export {
+  DefaultAppCommandService,
+  type CommandAuditClock,
+  type CommandAuditInputSummaryResolver,
+  type CommandRiskResolver,
+  type ContinueRunCommandHandler,
+  type DefaultAppCommandServiceOptions,
+  type ExecuteBuilderCommandHandler,
+  type RetryPhaseCommandHandler,
+  type StartRunCommandHandler
+} from "../../../src/application/commands/default-app-command-service.js";
+export {
+  EventedAppCommandService,
+  type CommandEventClock,
+  type CommandEventIdFactory,
+  type EventedAppCommandServiceOptions
+} from "../../../src/application/commands/evented-app-command-service.js";
 
-export * from "../../../src/application/events/app-event.js";
-export * from "../../../src/application/events/event-store.js";
-export * from "../../../src/application/events/in-memory-event-store.js";
+export { type AppEvent } from "../../../src/application/events/app-event.js";
+export { type AppEventBus } from "../../../src/application/events/app-event-bus.js";
+export { type AppEventStore } from "../../../src/application/events/event-store.js";
+export { InMemoryEventStore } from "../../../src/application/events/in-memory-event-store.js";
 
-export * from "../../../src/application/use-cases/add-task-comment-use-case.js";
-export * from "../../../src/application/use-cases/continue-run-use-case.js";
-export * from "../../../src/application/use-cases/execute-builder-use-case.js";
-export * from "../../../src/application/use-cases/mark-task-reviewed-use-case.js";
-export * from "../../../src/application/use-cases/retry-phase-use-case.js";
-export * from "../../../src/application/use-cases/select-task-use-case.js";
-export * from "../../../src/application/use-cases/start-run-use-case.js";
-export * from "../../../src/application/use-cases/update-coordination-note-use-case.js";
+export { DefaultAddTaskCommentUseCase, type AddTaskCommentUseCase } from "../../../src/application/use-cases/add-task-comment-use-case.js";
+export { DefaultContinueRunUseCase, type ContinueRunUseCase, type ContinueRunUseCaseHandler } from "../../../src/application/use-cases/continue-run-use-case.js";
+export { DefaultExecuteBuilderUseCase, type ExecuteBuilderUseCase, type ExecuteBuilderUseCaseHandler } from "../../../src/application/use-cases/execute-builder-use-case.js";
+export { DefaultMarkTaskReviewedUseCase, type MarkTaskReviewedUseCase } from "../../../src/application/use-cases/mark-task-reviewed-use-case.js";
+export { DefaultRetryPhaseUseCase, type RetryPhaseUseCase, type RetryPhaseUseCaseHandler } from "../../../src/application/use-cases/retry-phase-use-case.js";
+export { DefaultSelectTaskUseCase, type SelectTaskUseCase } from "../../../src/application/use-cases/select-task-use-case.js";
+export { DefaultStartRunUseCase, type StartRunUseCase, type StartRunUseCaseHandler } from "../../../src/application/use-cases/start-run-use-case.js";
+export { DefaultUpdateCoordinationNoteUseCase, type UpdateCoordinationNoteUseCase } from "../../../src/application/use-cases/update-coordination-note-use-case.js";
